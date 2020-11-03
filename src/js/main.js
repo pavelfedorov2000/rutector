3b7ef578615428d24c5ca7420656e610b2f43ad4
Pavel <pavelfedorov929@gmail.com>
2020-11-03 13:47:23 +0300

    правки

--- src/js/main.js
+++ src/js/main.js
@@ -395,12 +395,6 @@ $(function() {
         return false;
     });
 
-    /* $("a[href^='#']").click(function(){
-        const href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(href).offset().top+"px"}); // плавный скролл по ссылкам
-        return false;
-    }); */
-
     $('.form-style, .filter-style').styler();
 
 
